f72272a9577ca42b6f1ed3f63a194f71a2ee6a6d
lucaszales <zaleslucas3@gmail.com>
2026-05-07 08:44:19 +0000

    advanced

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+upper = False
+
+for letter in range(122, 96, -1):
+    char = chr(letter)
+
+
+    if upper:
+        char = char.upper()
+    else:
+        char = char.lower()
+
+    print(char, end="")
+
+    upper = not upper
