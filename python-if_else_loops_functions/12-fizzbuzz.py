6c843b449ef85488259a057589153258c8d6ee37
lucaszales <zaleslucas3@gmail.com>
2026-05-07 07:04:55 +0000

    task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-for numbers in range(1, 101):
-    if numbers % 3 == 0 and numbers % 5 == 0:
-        print("FizzBuzz", end=" ")
-    elif numbers % 3 == 0:
-        print("Fizz", end=" ")
-    elif numbers % 5 == 0:
-        print("Buzz", end=" ")
-    else:
-        print(numbers, end=" ")
-print("")
+def fizzbuzz():
+    for numbers in range(1, 101):
+        if numbers % 3 == 0 and numbers % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif numbers % 3 == 0:
+            print("Fizz", end=" ")
+        elif numbers % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(numbers, end=" ")
+    print("")
+fizzbuzz()
