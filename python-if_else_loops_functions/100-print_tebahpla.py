363626e0bfbf5989716770c5c248f9bf232b6e8b
lucaszales <zaleslucas3@gmail.com>
2026-05-07 08:48:58 +0000

    never doing advanced tasks again

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,13 +3,11 @@ upper = False
 
 for letter in range(122, 96, -1):
     char = chr(letter)
-
-
     if upper:
         char = char.upper()
     else:
         char = char.lower()
 
-    print(char, end="")
+    print("{}".format(char), end ="")
 
     upper = not upper
