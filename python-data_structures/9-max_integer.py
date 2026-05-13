d68746738635af4886f7546a62125220cf674ccb
lucas <zaleslucas3@gmail.com>
2026-05-13 12:11:56 +1000

    task 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    
+    maxm = my_list[0]
+    
+    for number in my_list:
+        if number > maxm:
+            maxm = number
+    return maxm
