81634914dd0e0c7f9efcf112c517605d83dc22a9
lucas <zaleslucas3@gmail.com>
2026-05-18 10:44:42 +1000

    task 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Module Prints Squares using #"""
+
 def print_square(size):
     """Prints square"""
-    
-    
+
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
 
     if size < 0:
         raise ValueError("size must be >= 0")
-   
+
     for i in range(size):
         print("#" * size)
