cb15102786053873d4ba18b6ac329ef45b3fd62e
lucas <zaleslucas3@gmail.com>
2026-05-19 11:29:43 +1000

    task3

---
+++ python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a class that defines a square"""
+
+
+class Square:
+    """an empy class with nothing"""
+
+    def __init__(self, size=0):
+        if size != int(size):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return self.__size **2
