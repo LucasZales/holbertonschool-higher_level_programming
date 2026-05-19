eca13e1ee8c5d25b5727cc8fc566a4741f290549
lucas <zaleslucas3@gmail.com>
2026-05-19 11:23:58 +1000

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,8 +6,8 @@ class Square:
     """an empy class with nothing"""
 
     def __init__(self, size=0):
-        self.__size = int(size)
         if size != int(size):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
