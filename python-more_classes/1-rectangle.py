41ef0b9df321e4e7d933a985e6f0c09a764fc81a
lucas <zaleslucas3@gmail.com>
2026-05-21 11:12:09 +1000

    added @ to the setter

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle:
     def width(self):
         return self.__width
 
-    width.setter
+    @width.setter
 
     def width(self, value):
         if type(value) is not int:
@@ -27,7 +27,7 @@ class Rectangle:
     def height(self):
         return self.__height
 
-    height.setter
+    @height.setter
 
     def height(self, value):
         if type(value) is not int:
