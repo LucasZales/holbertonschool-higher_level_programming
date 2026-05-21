85f49ba97c1dbd10b2b3992ec40789148cc6cf77
lucas <zaleslucas3@gmail.com>
2026-05-21 10:44:37 +1000

    task 6

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -5,17 +5,23 @@
 class Square:
     """an empy class with nothing"""
 
-    def __init__(self, size=0):
-        self.__size = size
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
 
     def area(self):
         return self.__size ** 2
 
     def my_print(self):
-        for i in range(self.size):
-            print("#" * self.size)
         if self.size == 0:
             print()
+            return
+
+        for i in range(self.position[1]):
+            print()
+
+        for i in range(self.size):
+            print(" " * self.position[0] + "#" * self.size)
 
     @property
     def size(self):
@@ -23,8 +29,24 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError("size must >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (
+            not isinstance(value, tuple)
+            or len(value) != 2
+            or not all(isinstance(num, int) and num >= 0 for num in value)
+        ):
+            raise TypeError(
+                "position must be a tuple of 2 positive integers"
+            )
+        self.__position = value
--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,8 +6,22 @@ class Square:
     """an empy class with nothing"""
 
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+            return
+
+        for i in range(self.position[1]):
+            print()
+
+        for i in range(self.size):
+            print(" " * self.position[0] + "#" * self.size)
 
     @property
     def size(self):
@@ -15,13 +29,10 @@ class Square:
 
     @size.setter
     def size(self, value):
-
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-
         if value < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = value
 
     @property
@@ -30,31 +41,12 @@ class Square:
 
     @position.setter
     def position(self, value):
-
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not isinstance(value[0], int) or
-                not isinstance(value[1], int) or
-                value[0] < 0 or
-                value[1] < 0):
-
+        if (
+            not isinstance(value, tuple)
+            or len(value) != 2
+            or not all(isinstance(num, int) and num >= 0 for num in value)
+        ):
             raise TypeError(
                 "position must be a tuple of 2 positive integers"
             )
-
         self.__position = value
-
-    def area(self):
-        return self.__size ** 2
-
-    def my_print(self):
-
-        if self.__size == 0:
-            print()
-            return
-
-        for i in range(self.__position[1]):
-            print()
-
-        for i in range(self.__size):
-            print((" " * self.__position[0]) + ("#" * self.__size))
