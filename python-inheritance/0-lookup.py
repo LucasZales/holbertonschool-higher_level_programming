f6be7ae2b42db7ba7c28f6ccdef73c1ad8815455
lucas <zaleslucas3@gmail.com>
2026-05-25 09:14:38 +1000

    task 0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""returns the list of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """returns a list of attributes and methods"""
+    return dir(obj)
