7d896252cee94b77fa6787bf8150da26c58813ff
lucas <zaleslucas3@gmail.com>
2026-05-26 14:23:19 +1000

    cleaning

---
+++ pruebas.py/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list):
+        return None
+    
+    new_list = my_list[:]
+    for num in new_list:
+        if num == idx:
+            new_list[num] = element
+    return(new_list)
+    
