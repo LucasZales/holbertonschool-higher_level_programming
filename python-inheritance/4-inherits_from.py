6a7b832bfe158e6da89300106aaf2ea61b2dca8e
lucas <zaleslucas3@gmail.com>
2026-05-28 13:14:13 +1000

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""returns True if the object is an instance of the specified class"""
+
+
 def inherits_from(obj, a_class):
+    """checks if an object is an instance of a class or inherited class"""
     return isinstance(obj, a_class) and type(obj) is not a_class
