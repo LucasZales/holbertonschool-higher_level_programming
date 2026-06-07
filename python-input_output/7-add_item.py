4c4e736bed5fe5beaf32a737183243434b5215ca
lucas <zaleslucas3@gmail.com>
2026-06-07 10:29:16 +1000

    task 7 .json / _json

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,7 +6,7 @@ from sys import argv
 save = __import__("5-save_to_json_file").save_to_json_file
 load = __import__("6-load_from_json_file").load_from_json_file
 
-filename = "add_item_json"
+filename = "add_item.json"
 
 try:
     items = load(filename)
