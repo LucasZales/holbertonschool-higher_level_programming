ba5a4f1bb0310b5cb7692bf7d513c8f7065474eb
lucas <zaleslucas3@gmail.com>
2026-06-13 10:36:39 +1000

    task 2

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import csv
+import requests
+
+URL = "https://jsonplaceholder.typicode.com/posts"
+
+def fetch_and_print_posts():
+    response = requests.get(URL)
+    print(response)
+    if response.status_code == 200:
+        posts = response.json()
+        
+        for post in posts:
+            print(post['title'])
+        
+fetch_and_print_posts()
+
+def fetch_and_save_posts():
+    response = requests.get(URL)
+    print(response)
+    if response.status_code == 200:
+        posts = response.json()
+        
+        info = []
+        for post in posts:
+            info.append({
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            })
+        
+        with open("posts.csv", "w" , newline="") as csvfile:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            
+            writer.writeheader()
+            writer.writerows(info)
+    
