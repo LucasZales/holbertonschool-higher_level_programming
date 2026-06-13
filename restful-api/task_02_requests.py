98c319eca752e2488b7e8c2c1dbcd4d8faa7fa39
lucas <zaleslucas3@gmail.com>
2026-06-13 10:38:59 +1000

    task 2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,7 +6,7 @@ URL = "https://jsonplaceholder.typicode.com/posts"
 
 def fetch_and_print_posts():
     response = requests.get(URL)
-    print(response)
+    print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
         posts = response.json()
         
@@ -17,7 +17,7 @@ fetch_and_print_posts()
 
 def fetch_and_save_posts():
     response = requests.get(URL)
-    print(response)
+    print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
         posts = response.json()
         
