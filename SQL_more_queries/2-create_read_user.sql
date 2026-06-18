ec80ab598d69d9bd18c2bc87ab8075108756e41e
lucas <zaleslucas3@gmail.com>
2026-06-18 14:31:25 +1000

    task 2 fixed

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- creating database and user2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO user_0d_2@localhost;
 FLUSH PRIVILEGES; 
