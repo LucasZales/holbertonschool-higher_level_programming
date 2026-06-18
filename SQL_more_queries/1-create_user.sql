fdf1164260a9aabe72b7bd59def6ea946de8b2ed
lucas <zaleslucas3@gmail.com>
2026-06-18 14:01:38 +1000

    task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates a server user
 CREATE USER IF NOT EXISTS user_0d_1@localhost 
 IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd'
-GRANT ALL PRIVILEGES ON hbtn_0c_0.* TO user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1
 FLUSH PRIVILEGES;
