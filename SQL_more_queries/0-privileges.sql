ea4baa20300f7348bdcc5c75fcad008a3ceb8a20
lucas <zaleslucas3@gmail.com>
2026-06-18 14:00:05 +1000

    task 1

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- lists all privileges of MYSQL usersw
 SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR user_0d_2@localhostl
--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,5 +2,4 @@
 CREATE USER IF NOT EXISTS user_0d_1@localhost 
 IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd'
 GRANT ALL PRIVILEGES ON hbtn_0c_0.* TO user_0d_1
-CREATE USER IF NOT EXITS user_0d_2@localhost
-IDENTIFIED WITH authentication_plugin BY 'user_0d_2_pwd'
+FLUSH PRIVILEGES;
