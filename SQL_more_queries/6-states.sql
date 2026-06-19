beef8abc57109094403fb9e024a0265882d98ead
lucas <zaleslucas3@gmail.com>
2026-06-19 15:01:09 +1000

    task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Create a database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Create a tabla states within database
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL);
