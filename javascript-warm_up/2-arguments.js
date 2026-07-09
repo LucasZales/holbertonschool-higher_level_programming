f3ed26650d9ba6d99a4641f4c0eb710fabe42017
LucasZales <zaleslucas3@gmail.com>
2026-07-10 09:44:04 +1000

    task 2 fixed

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const count = process.argv.length -2;
+const count = process.argv.length - 2;
 
 if (count === 0) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (count === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else if (count > 1) {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
