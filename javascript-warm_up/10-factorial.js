35b507e0da080640dee2d8d6ca0ca75629f41ddd
LucasZales <zaleslucas3@gmail.com>
2026-07-14 12:17:13 +1000

    betty?

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -2,12 +2,12 @@
 
 const args = parseInt(process.argv[2]);
 
-function factorial(num) {
-    if (isNaN(num) || num <= 1) {
-        return 1;
-    } else {
-        return num * factorial(num -1);
-    }
+function factorial (num) {
+  if (isNaN(num) || num <= 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 }
 
-    console.log(factorial(args));
+console.log(factorial(args));
