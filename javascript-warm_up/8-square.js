384bcec832e9e2e6e9890db493b755e5b95e2375
LucasZales <zaleslucas3@gmail.com>
2026-07-14 11:35:39 +1000

    task 8

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const veces = parseInt(args[0]);
+
+if (args[0] === undefined) {
+  console.log('Missing size');
+} else if (!isNaN(veces)) {
+  for (let fila = 0; fila < veces; fila++) {
+    console.log('x'.repeat(veces));
+  }
+}
