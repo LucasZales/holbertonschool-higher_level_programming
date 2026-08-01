449fd6b5ed7d551c60dea222aea51955a35e6962
LucasZales <zaleslucas3@gmail.com>
2026-08-02 09:57:47 +1000

    task 3

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Flask app with a page that shows items from a JSON file
+"""
+
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """Home page"""
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    """About page"""
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    """Contact page"""
+    return render_template("contact.html")
+
+
+@app.route("/items")
+def items():
+    """Show items from JSON"""
+
+    with open("items.json", "r") as file:
+        data = json.load(file)
+
+    return render_template("items.html", items=data["items"])
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
