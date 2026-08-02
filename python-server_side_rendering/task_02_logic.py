5cf35b28b5304604d33362755e92827527579063
LucasZales <zaleslucas3@gmail.com>
2026-08-02 10:02:26 +1000

    task 2 fixed

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -34,7 +34,8 @@ def items():
     with open("items.json", "r") as file:
         data = json.load(file)
 
-    return render_template("items.html", items=data["items"])
+    items = data.get("items", [])
+    return render_template("items.html", items=items)
 
 
 if __name__ == "__main__":
